7e3ff9242e178642ef467e84cb14ba9251b2d464
Dheeraj Reddy <meetdheerajreddy@gmail.com>
2022-09-26 19:47:07 +0530

    first

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <span>
+            <p>&copy; 2022 | Designed and Built by Team Horus</p>
+            <p>Dheeraj | Bhargav</p>
+        </span>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/pages/Home.js
@@ -0,0 +1,63 @@
+import React from 'react'
+// import Glass from '../components/glass'
+import '../styles/Home.css'
+import { FaArrowDown, FaBars } from 'react-icons/fa'
+import Story1 from './Story1'
+import Story2 from './Story2'
+import SpaceVideo from '../assets/spacevid.mp4'
+
+function Home() {
+
+  function MenuToggle() {
+    const menu = document.querySelector('.toggle');
+    menu.classList.toggle('active');
+
+    const section = document.querySelector('.section');
+    section.classList.toggle('active');
+
+    const bars = document.querySelector('.bars');
+    bars.classList.toggle('active');
+
+    const home = document.querySelector('.home');
+    home.classList.toggle('active');
+  }
+
+  return (
+    <div className="home">
+      <div className='section'>
+        <div className='header'>
+          <a href='/' className='logo'>Space.</a>
+          <div className='toggle' onClick={MenuToggle}>
+            <FaBars size={"25px"} color="white" className='bars'/>
+          </div>
+        </div>
+        <div className='glassnav'></div>
+        <div className='content'>
+          <h2>Parker Solar Probe<br /><span>A story to be told...</span></h2>
+        </div>
+        <ul className='sci'>
+          <li><a href='https://blogs.nasa.gov/parkersolarprobe/2018/08/11/the-parker-solar-probe-mission/' target='_blank' rel="noreferrer">NASA</a></li>
+          <li><a href='/'>Github</a></li>
+        </ul>
+        <ul className='navigation'>
+          <li><a href='/'>Option1</a></li>
+          <li><a href='/'>Option2</a></li>
+          <li><a href='/aboutus'>About the Team</a></li>
+        </ul>
+        <div className='scrolldown'>
+          <h2>Scroll Down</h2>
+          <br />
+          <FaArrowDown size={"25px"} className='arrow'/>
+        </div>
+      </div>
+      <div className='story1'>
+        <Story1 />
+      </div>
+      <div className='story2'>
+        <Story2 />
+      </div>
+    </div>
+  )
+}    
+
+export default Home
